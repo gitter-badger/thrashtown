f56b217394683fdcaf175ce38720af943939de26
Ramin Taleghani <talegram@gmail.com>
2014-05-19 12:20:58 -0700

    Changes placeholder schema of 'Thing' with 'Surf' and refactors where needed.

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -2,7 +2,7 @@
 
 angular.module('hackathonApp')
   .controller('MainCtrl', function ($scope, $http) {
-    $http.get('/api/awesomeThings').success(function(awesomeThings) {
-      $scope.awesomeThings = awesomeThings;
+    $http.get('/api/surfs').success(function(surfs) {
+      $scope.surfs = surfs;
     });
   });
