3bbb880b018e9032c55495c84f40521eb8b08946
Ramin Taleghani <talegram@gmail.com>
2014-11-20 01:25:32 -0800

    Rebuilds scaffold using latest fullstack generator

---
+++ client/app/main/main.js
@@ -0,0 +1,11 @@
+'use strict';
+
+angular.module('thrashtownApp')
+  .config(function ($stateProvider) {
+    $stateProvider
+      .state('main', {
+        url: '/',
+        templateUrl: 'app/main/main.html',
+        controller: 'MainCtrl'
+      });
+  });
